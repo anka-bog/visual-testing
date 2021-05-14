5ae4339ac90b9287a42dfbd80fd9cfa9b99c7d77
AniaOssowska <anka.bogacka@hotmail.com>
2021-05-14 10:09:43 +0200

    Multiple pages testing and clock settings

--- cypress/integration/image.spec.js
+++ cypress/integration/image.spec.js
@@ -1,8 +1,17 @@
+const pages = ['http://example.com/']
+
+const sizes = ['iphone-6', 'ipad-2', [1200, 800]]
+
 describe('Visual Regression', () => {
-  it('My first visual regression test', () => {
-    //load website
-    cy.visit('http://example.com')
-    //compare snapshots
-    cy.matchImageSnapshot()
+  sizes.forEach(size => {
+    pages.forEach(page => {
+      it(`Should match ${page} in resolution ${size}`, () => { //backquote here!!!
+        let currentTime = new Date(Date.UTC(2020, 1, 1)).getDate()
+        cy.clock(currentTime)
+        cy.setResolution(size)
+        cy.visit(page)
+        cy.matchImageSnapshot()
+      })
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,12 +2,12 @@ import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command"
 
 addMatchImageSnapshotCommand({
   failureTreshold: 0.0, //pixel perfect, no tolerance for ecen one pixel
-  failurTresholdType: 'percent',
+  failurTresholdType: "percent",
   customDiffConfig: { treshold: 0.0 },
-  capture: 'viewport'
+  capture: "viewport"
 })
 
-Cypress.Commands.add('setResolution', (size) => {
+Cypress.Commands.add("setResolution", size => {
   if (Cypress._.isArray(size)) {
     cy.viewport(size[0], size[1])
   } else {
